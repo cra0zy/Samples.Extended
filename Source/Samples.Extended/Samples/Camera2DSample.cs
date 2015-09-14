0d5e00c8f6afc05e5b9d0e9ef9df6c846b9d2b92
Dylan Wilson <dylan@craftworkgames.com>
2015-09-14 17:54:21 +1000

    more work on the camera sample

--- Source/Samples.Extended/Samples/Camera2DSample.cs
+++ Source/Samples.Extended/Samples/Camera2DSample.cs
@@ -2,6 +2,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGame.Extended;
+using MonoGame.Extended.BitmapFonts;
 
 namespace Samples.Extended.Samples
 {
@@ -31,6 +32,7 @@ namespace Samples.Extended.Samples
 
         protected override void LoadContent()
         {
+            _bitmapFont = Content.Load<BitmapFont>("montserrat-32");
             _backgroundSky = Content.Load<Texture2D>("hills-sky");
             _backgroundClouds = Content.Load<Texture2D>("hills-clouds");
 
@@ -47,17 +49,29 @@ namespace Samples.Extended.Samples
         {
         }
 
+        private float _cloudsOffset;
+        private BitmapFont _bitmapFont;
+        private const float _cloudsRepeatWidth = 800;
+
         protected override void Update(GameTime gameTime)
         {
             var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
-            
+
+            _cloudsOffset -= deltaTime * 5;
+
+            if (_cloudsOffset < -_cloudsRepeatWidth)
+                _cloudsOffset = _cloudsRepeatWidth;
+
             var keyboardState = Keyboard.GetState();
             var mouseState = Mouse.GetState();
             
             if (keyboardState.IsKeyDown(Keys.Escape))
                 Exit();
 
-            const float movementSpeed = 100;
+            // the camera properties of the camera can be conrolled to move, zoom and rotate
+            const float movementSpeed = 200;
+            const float rotationSpeed = 0.5f;
+            const float zoomSpeed = 0.5f;
 
             if (keyboardState.IsKeyDown(Keys.W) || keyboardState.IsKeyDown(Keys.Up))
                 _camera.Move(new Vector2(0, -movementSpeed) * deltaTime);
@@ -71,6 +85,18 @@ namespace Samples.Extended.Samples
             if (keyboardState.IsKeyDown(Keys.D) || keyboardState.IsKeyDown(Keys.Right))
                 _camera.Move(new Vector2(movementSpeed, 0) * deltaTime);
 
+            if (keyboardState.IsKeyDown(Keys.E))
+                _camera.Rotation += rotationSpeed * deltaTime;
+
+            if (keyboardState.IsKeyDown(Keys.Q))
+                _camera.Rotation -= rotationSpeed * deltaTime;
+
+            if (keyboardState.IsKeyDown(Keys.R))
+                _camera.Zoom += zoomSpeed * deltaTime;
+
+            if (keyboardState.IsKeyDown(Keys.F))
+                _camera.Zoom -= zoomSpeed * deltaTime;
+
             base.Update(gameTime);
         }
 
@@ -78,24 +104,38 @@ namespace Samples.Extended.Samples
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            _spriteBatch.Begin(transformMatrix: _camera.GetViewMatrix(Vector2.Zero));
+            // the camera produces a view matrix that can be applied to any sprite batch
+            var transformMatrix = _camera.GetViewMatrix(Vector2.Zero);
+            _spriteBatch.Begin(transformMatrix: transformMatrix);
             _spriteBatch.Draw(_backgroundSky, Vector2.Zero, Color.White);
-            _spriteBatch.Draw(_backgroundClouds, Vector2.Zero, Color.White);
+            _spriteBatch.Draw(_backgroundClouds, new Vector2(-_cloudsOffset, 0), Color.White);
+            _spriteBatch.Draw(_backgroundClouds, new Vector2(_cloudsOffset, 10), Color.White);
             _spriteBatch.End();
 
             for (var layerIndex = 0; layerIndex < 4; layerIndex++)
             {
-                var viewMatrix = _camera.GetViewMatrix(Vector2.One * (0.25f * layerIndex));
+                // different layers can have a parallax factor applied for a nice depth effect
+                var parallaxFactor = Vector2.One * (0.25f * layerIndex);
+                var viewMatrix = _camera.GetViewMatrix(parallaxFactor);
                 _spriteBatch.Begin(transformMatrix: viewMatrix);
                 
                 for (var repeatIndex = -3; repeatIndex <= 3; repeatIndex++)
                 {
-                    var position = new Vector2(repeatIndex * 800, 0);
-                    _spriteBatch.Draw(_backgroundHills[layerIndex], position, Color.White);
+                    var texture = _backgroundHills[layerIndex];
+                    var position = new Vector2(repeatIndex * texture.Width, 0);
+                    _spriteBatch.Draw(texture, position, Color.White);
                 }
 
                 _spriteBatch.End();
             }
+
+            // not all sprite batches need to be affected by the camera
+            _spriteBatch.Begin();
+            _spriteBatch.DrawString(_bitmapFont, 
+                "WASD: move\n" +
+                "RF: zoom in and out", 
+                new Vector2(5,5), Color.DarkBlue);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
