25b0316ef50c74bcea0929d7148042dec9bda7ea
Dylan Wilson <dylan@craftworkgames.com>
2015-12-16 12:36:19 +1000

    replaced the launcer with a rebuilt project

--- Source/Launcher/Program.cs
+++ Source/Launcher/Program.cs
@@ -1,16 +1,23 @@
-﻿using Samples.Extended;
+﻿using System;
+using Samples.Extended;
 
-namespace Launcher
+namespace Game2
 {
-    static class Program
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
     {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
         static void Main()
         {
             using (var game = new Game1())
-            {
                 game.Run();
-            }
         }
     }
+#endif
 }
-
